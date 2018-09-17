1033f9688b8cf9c171a0a73cecb455830493cb01
Tatsiana Sakalinskaya <tani4ik@gmail.com>
2018-09-17 20:50:15 +0300

    Love triangle

--- src/index.js
+++ src/index.js
@@ -3,5 +3,15 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  let count = 0;
+  for (var i = 0; i < preferences.length; i++) {
+     let SpichoneeA = i + 1;
+     let SpichoneeB = preferences[SpichoneeA - 1];
+     let SpichoneeC = preferences[SpichoneeB - 1];
+     let SpichoneeCLoves = preferences[SpichoneeC - 1];
+     if (SpichoneeA === SpichoneeCLoves) {
+        count++;
+     }
+  }
+  return Math.floor(count/3);
 };
